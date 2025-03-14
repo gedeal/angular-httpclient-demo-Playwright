dc2a3eb2de69e84ece9186f7f7255be419d4e802
Gerson <gerson_a@hotmail.com>
2025-03-14 19:16:20 +0100

    upd 1

---
+++ e2e/test_4_US_1.spec.js
@@ -0,0 +1,62 @@
+//#  ## [User story 1]
+//#  Som en användare, vill jag se mötet "sprint planning", som utspelar sig
+//#  första dagen i sprinten, så att jag vet vad jag ska göra på mötet.
+
+import { test, expect } from '@playwright/test';
+
+import * as allure from "allure-js-commons";
+import { ContentType } from "allure-js-commons";
+
+test('has title', async ({ page }) => {
+    // Resize viewport
+    //const viewport = page.viewportSize();
+    //const newWidth = viewport.width -800;
+    //const newHeight = viewport.height -500;
+    // Set the new viewport size
+    //await page.setViewportSize({ width: newWidth, height: newHeight });
+    
+  // Test
+    await page.goto('https://lejonmanen.github.io/agile-helper/');
+
+  // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle("Agile helper");
+
+    await page.screenshot({ path: 'allure-report/screenshot_1.png' });
+    await allure.attachmentPath("Screenshot 1", "allure-report/screenshot_1.png", {
+      contentType: ContentType.PNG,
+      fileExtension: "png",
+      scale:'css', 
+      quality: 50
+    });
+
+    await page.getByRole('button', { name: 'First' }).click();
+
+    await page.getByRole('button', { name: 'Sprint planning' }).click();
+
+    //await expect(page).tobevisible("Sprint planning")
+    await expect(page.locator('h2')).toContainText("Sprint planning");
+
+    await page.screenshot({ path: 'allure-report/screenshot_2.png' });
+    await allure.attachmentPath("Screenshot_2", "allure-report/screenshot_2.png", {
+      contentType: ContentType.PNG,
+      fileExtension: "png",
+    });
+
+    await page.getByRole('button', { name:"Ok we're done. Start the sprint!"}).click();
+
+    await expect(page.getByRole('button',{name:" Start over"})).toBeVisible
+
+    await page.screenshot({ path: 'allure-report/screenshot_3.png' });
+
+
+    await allure.attachment("Test 1-Text file", "This is the file content.", ContentType.TEXT);
+
+    await allure.attachmentPath("Screenshot 3", "allure-report/screenshot_3.png", {
+      contentType: ContentType.PNG,
+      fileExtension: "png",
+    });
+
+
+
+});
+
---
+++ runbash.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#clear
+echo "*****************************************************************************************"
+echo "Running Startgate Tests :  $(date +"%A, %Y/%m/%d - %H:%M")"
+#echo "(By Gerson Almeida - 2023/10)"
+echo "*****************************************************************************************"
+echo ""
+Tpwd=$(pwd)
+echo "Tpwd dir:  " $Tpwd
+
+echo ""
+rm -r $Tpwd/allure-results/*
+rm -r $Tpwd/allure-report/*
+#echo 'adfs_name:  ' $1
+
+
+echo "****************************************************************************************"
+echo ""
+
+
+#npx playwright test --ui
+
+npx playwright test
+echo
+echo "=======================> End Tests Cases"
+echo
+
+
+
+##--Allure-------------------------------------------------------------------------------------------
+
+#npx allure serve allure-results
+
+
+npx allure generate --clean $Tpwd/allure-results -o $Tpwd/allure-report  --single-file --name "Exempel_Gerson" --lang "en"
+
+start chrome --new-window  $Tpwd/allure-report/index.html
+
+echo "*****************************************************************************************"
+echo "Finished Running Tests :  $(date +"%A, %Y/%m/%d - %H:%M")"
+echo "*****************************************************************************************"
+
